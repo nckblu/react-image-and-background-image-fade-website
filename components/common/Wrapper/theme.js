9f2e90e6363ea192e7eac8308c4ac755e3a96566
Nicholas Porter <web@nck.me.uk>
2020-02-26 21:25:54 +0000

    Changed header and added breakpoints for styled components

--- components/common/Wrapper/theme.js
+++ components/common/Wrapper/theme.js
@@ -1,6 +1,13 @@
 export const theme = {
   main: "#000",
   light: "#fff",
+  breakpoints: {
+    xs: 0,
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
+  },
 };
 
 export default theme;
--- components/home/Header/elements/Header.js
+++ components/home/Header/elements/Header.js
@@ -1,8 +1,15 @@
 import styled from "styled-components";
+import { breakpoint } from "styled-components-breakpoint";
 
 export const Header = styled.div`
   height: calc(100vh - 40px);
   min-height: 400px;
+  border: 30px solid white;
+  margin-top: 76px;
+  
+  // ${breakpoint("sm")`
+  //   display: none;
+  // `}
 `;
 
 export default Header;
